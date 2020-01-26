4b670f36f2ec769882e3f85fb045a6067cb218fd
Antonio <azamora@telstock.net>
2020-01-25 20:09:09 -0600

    Proyecto Valida CDFI

---
+++ Controllers/ValidarCFDIController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ValidacionCFDI.Models;
+
+namespace ValidacionCFDI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidarCFDIController : ControllerBase
+    {
+        [HttpPost]
+        public IEnumerable<string> PostJsonString([FromBody] Response request)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ValidacionCFDI.Models
+{
+    public class Response
+    {
+        public string RFC { get; set; }
+        public XmlDocument CFDI { get; set; }
+    }
+}
